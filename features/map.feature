a592ce897d4b98a7118bbdff348aefd4e034a273
tom1299 <tom1299@jeeatwork.com>
2022-05-16 14:12:30 +0200

    Add missing gherkin steps

--- features/map.feature
+++ features/map.feature
@@ -15,20 +15,20 @@ Scenario: Customized map creation
   When I create a customized map
   Then the maps size is 10 x 3
   And the store is located at 1,1
-  And plots of type "river" are located at:
-    | x  | y |
-    | 2  | 1 |
-    | 2  | 2 |
-    | 2  | 3 |
-  And plots of type "mountain" are randomly distributed
+  And the map contains the following plots:
+    | x  | y | type  |
+    | 2  | 1 | river |
+    | 2  | 2 | river |
+    | 2  | 3 | river |
+  And plots of type mountain are randomly distributed
   And I finish map creation
   Then the maps size should be 10 x 3
   And the store should be located at 1,1
-  And plots of type "river" should be located at:
-    | x  | y |
-    | 2  | 1 |
-    | 2  | 2 |
-    | 2  | 3 |
+  And the map contains the following plots:
+    | x  | y | type  |
+    | 2  | 1 | river |
+    | 2  | 2 | river |
+    | 2  | 3 | river |
 
 Scenario: Customized map creation without mandatory store
   When I create a customized map
--- features/steps/steps.py
+++ features/steps/steps.py
@@ -14,22 +14,61 @@ def create_a_map(context, map_type):
     pass
 
 
-@step('the maps size is (\\d+) x (\\d+)')
-def map_size_is(context, x: int, y: int):
+@step('I finish map creation')
+def finish_map_creation(context):
+    """
+    :type context: behave.runner.Context
+    """
+    pass
+
+
+@step('the maps size (is|should be) (\\d+) x (\\d+)')
+def map_size_is(context, is_or_should: str, x: int, y: int):
     """
     :param y: The y size
     :param x: The x size
+    :param is_or_should: Whether the size is or should be
     :type context: behave.runner.Context
     """
     # raise NotImplementedError(f'STEP: the maps size is {x} x {y}')
     pass
 
-@step('the store is located at (\\d+),(\\d+)')
-def store_is_located_at(context, x: int, y: int):
+
+@step('the store (is|should be) located at (\\d+),(\\d+)')
+def store_is_located_at(context, is_or_should: str, x: int, y: int):
     """
+    :param is_or_should: Whether the store is or should be located
     :param y: The y size
     :param x: The x size
     :type context: behave.runner.Context
     """
     # raise NotImplementedError(f'STEP: the store is located at {x} x {y}')
     pass
+
+
+@step('the map contains the following plots')
+def map_contains_plots(context):
+    """
+    :type context: behave.runner.Context
+    """
+    pass
+
+
+@step('plots of type (mountain|river) are randomly distributed')
+def map_contains_plots(context, plot_type: str):
+    """
+    :param plot_type: The type of the plot
+    :type context: behave.runner.Context
+    """
+    pass
+
+
+@step('the error "(.*)" should occur')
+def error_should_occur(context, error: str):
+    """
+    :param error: The error
+    :type context: behave.runner.Context
+    """
+    pass
+
+
