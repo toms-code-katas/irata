4789ea5d4270ee0d577e5f68695d8d57e7fae0ed
tom1299 <tom1299@jeeatwork.com>
2022-05-18 08:21:26 +0200

    Add creation of map and loading of maps from json

--- irata/model.py
+++ irata/model.py
@@ -1,5 +1,33 @@
 from enum import Enum
-from irata import utils
+import json
+import os
+
+
+def to_plot(json_values):
+    # TODO: Rethink json to object serialization
+    if "x" in json_values and "y" in json_values:
+        return Coordinates(int(json_values["x"]), int(json_values["y"]))
+    elif "coordinates" in json_values:
+        return Plot(coordinates=json_values["coordinates"])
+    else:
+        raise NotImplementedError(f"Could not deserialized dict {json_values} to any type")
+
+
+def to_dict(o: object) -> {}:
+    return o.__dict__
+
+
+def to_json(o: object) -> str:
+    to_serialize = o
+    if isinstance(o, list):
+        to_serialize = [ob.__dict__ for ob in o]
+    return json.dumps(to_serialize, default=to_dict, indent=2)
+
+
+def load_map(name: str = "default"):
+    map_file_name = os.path.dirname(os.path.abspath(__file__)) + "/maps/" + name + ".json"
+    with open(map_file_name, 'r') as map_file:
+        return json.loads(map_file.read(), object_hook=to_plot)
 
 
 class Player:
@@ -32,11 +60,25 @@ class Map:
 
     __plots = {}
 
-    def __init__(self, width: int = 9, height: int = 5):
+    def __init__(self, width: int = -1, height: int = -1):
         self.width = width
         self.height = height
 
     def create(self):
+        if self.is_default_map():
+            self.create_default_map()
+        else:
+            self.create_custom_map()
+
+    def create_default_map(self):
+        list_of_plots = load_map("default")
+        for plot in list_of_plots:
+            self.__plots[plot.coordinates] = plot
+        last_plot = list(self.__plots.keys())[-1]
+        self.width = last_plot.x
+        self.height = last_plot.y
+
+    def create_custom_map(self):
         for x in range(self.width):
             for y in range(self.height):
                 coordinates = Coordinates(x + 1, y + 1)
@@ -45,6 +87,9 @@ class Map:
 
     def get_plots(self):
         return self.__plots.values()
+
+    def is_default_map(self):
+        return self.width == -1 and self.height == -1
 
 
 class LandGrant:
